038a743adf378bb1fccd2ec6dacbaebe4f655802
Petros <petros.rodinos@yahoo.com>
2026-02-03 17:13:30 +0200

    narcs positioning

--- app/src/pages/Game/utils/narcUtils.ts
+++ app/src/pages/Game/utils/narcUtils.ts
@@ -9,6 +9,10 @@ export const getNarcPositions = (bomberPos: Position): Position[] => {
     const { row, col } = bomberPos
 
     const diagonalPositions: Position[] = [
+        { row: row - 1, col: col - 1 },
+        { row: row - 1, col: col + 1 },
+        { row: row + 1, col: col - 1 },
+        { row: row + 1, col: col + 1 },
         { row: row - 2, col: col - 2 },
         { row: row - 2, col: col + 2 },
         { row: row + 2, col: col - 2 },
